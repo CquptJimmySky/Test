32dfa1b974a310779c3c72ff33ad65a7848467fd
CquptJimmySky <121420023@qq.com>
2015-03-21 10:40:37 +0800

    third commit

---
+++ BallRunnable.java
@@ -0,0 +1,3 @@
+
+public class BallRunnable implements Runnable{
+}
---
+++ BounceThread.java
@@ -0,0 +1,20 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+
+public class BounceThread {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				JFrame frame = new BounceFrame();
+				frame.setTitle("BounceThread");
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+			}
+		});
+	}
+}
